7b4f4d5d9875e9660f03df55aa5eaff1c407e90f
LPTimey <tim.ruland@icloud.com>
2023-06-25 17:32:21 +0200

    added template for landing page

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use leptos::*;
+use leptos::{ev::MouseEvent, *};
 use leptos_meta::*;
 use leptos_router::*;
 
@@ -15,15 +15,13 @@ pub fn App(cx: Scope) -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Tim Ruland"/>
 
         // content for this welcome page
         <Router>
-            <main>
                 <Routes>
                     <Route path="" view=|cx| view! { cx, <HomePage/> }/>
                 </Routes>
-            </main>
         </Router>
     }
 }
@@ -31,12 +29,40 @@ pub fn App(cx: Scope) -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! { cx,
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        //<!-- Start Landing Page https://codepen.io/Mohamed-Anwar97/pen/yLOwGed -->
+        <div class="landing-page">
+            <Header/>
+            <main class="content">
+                <div class="container">
+                    <div class="info">
+                        <h1>"Looking For Inspiration"</h1>
+                        <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil"</p>
+                        <button>"Button name"</button>
+                    </div>
+                    <div class="image">
+                        <img src="https://i.postimg.cc/65QxYYzh/001234.png"/>
+                    </div>
+                </div>
+            </main>
+        </div>
+        //<!-- End Landing Page -->
+    }
+}
+#[component]
+pub fn Header(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <header>
+            <div class="container">
+                <a href="#" class="logo">"Your" <b>"Website"</b></a>
+                <ul class="links">
+                    <li><a href="#">"Home"</a></li>
+                    <li><a href="#">"About Us"</a></li>
+                    <li><a href="#">"Work"</a></li>
+                    <li><a href="#">"Info"</a></li>
+                    <li><a href="#">"Get Started"</a></li>
+                </ul>
+            </div>
+        </header>
     }
 }
