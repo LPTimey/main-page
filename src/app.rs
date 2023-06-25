81292b4060b4a38a8fe2f3c80b023b6e5b083acc
LPTimey <tim.ruland@icloud.com>
2023-06-25 20:37:06 +0200

    made multiple articles functional

--- src/app.rs
+++ src/app.rs
@@ -44,6 +44,16 @@ fn HomePage(cx: Scope) -> impl IntoView {
                         <img src="https://i.postimg.cc/65QxYYzh/001234.png"/>
                     </div>
                 </article>
+                <article class="container">
+                    <div class="info">
+                        <h1>"Looking For Inspiration 2"</h1>
+                        <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil"</p>
+                        <button>"Button name 2"</button>
+                    </div>
+                    <div class="image">
+                        <img src="https://i.postimg.cc/65QxYYzh/001234.png"/>
+                    </div>
+                </article>
             </main>
             <Footer/>
         </div>
